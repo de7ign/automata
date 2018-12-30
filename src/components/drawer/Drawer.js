3981c4916eafe9cfdace08f78f45d7c8db8839ed
Nihal Murmu <nhlmrm@gmail.com>
2018-12-31 01:09:13 +0530

    Create Drawer.js
    a drawer will open from side when menu-button in Header component is cliced

---
+++ src/components/drawer/Drawer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Drawer extends Component {
+  render() {
+    return (
+      <div>
+        Drawer
+      </div>
+    )
+  }
+}
+
+export default Drawer;
