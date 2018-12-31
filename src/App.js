050765b38657147e3751beb01e89d2a4ffd17ed7
Nihal Murmu <nhlmrm@gmail.com>
2018-12-31 22:11:26 +0530

    Update Canvas.js, App.js & App.css | Delete Canvas.css
    Updated Canvas component and imported in App, Canas.css is deleted and inline-css is used.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import './App.css';
-import TemporaryDrawer from './components/drawer/TemporaryDrawer';
+import Canvas from './components/canvas/Canvas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <TemporaryDrawer />
+        <Canvas />
         <Footer />
       </div>
     );
