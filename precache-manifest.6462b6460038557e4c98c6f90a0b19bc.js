c73e8fecfe3d2dad6bf4dc8c941feee9671c4e2b
Nihal Murmu <nhlmrm@gmail.com>
2019-05-15 03:01:20 +0530

    Updates

---
+++ precache-manifest.6462b6460038557e4c98c6f90a0b19bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2149900b3b209948e7a",
+    "url": "/automata/static/js/runtime~main.a7eb554e.js"
+  },
+  {
+    "revision": "4080f3b655f34f626464",
+    "url": "/automata/static/js/main.32223efb.chunk.js"
+  },
+  {
+    "revision": "77083d3680750970bbcc",
+    "url": "/automata/static/js/2.843e3bbe.chunk.js"
+  },
+  {
+    "revision": "4080f3b655f34f626464",
+    "url": "/automata/static/css/main.a1ff8d39.chunk.css"
+  },
+  {
+    "revision": "5d7772a1f9891a4330a57f2f1febfef7",
+    "url": "/automata/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/automata/precache-manifest.0d4d752cb364701f96091e08ff0baaf8.js"
+  "/automata/precache-manifest.6462b6460038557e4c98c6f90a0b19bc.js"
 );
 
 workbox.clientsClaim();
