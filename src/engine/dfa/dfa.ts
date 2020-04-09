c5d988515d6045fa3ea63fb9a599f1e1cc6e29a5
Nihal Murmu <nhlmrm@gmail.com>
2020-04-09 23:08:15 +0530

    feat(engine) : Add validator for DFA (#91)
    Added a validator, which will check if the given network is valid DFA or not.

--- src/engine/dfa/dfa.ts
+++ src/engine/dfa/dfa.ts
@@ -1,8 +1,33 @@
 import { DataSet } from "vis-data/peer/esm/vis-data";
 
+/*
+NODE Data Model
+    {
+      id: "1",        - string
+      label: "start", - string
+      final: true,    - boolean (optional field, absence of field will be treated as false)
+      x: -184,        - number
+      y: -41          - number
+    }
+
+EDGE Data Model
+    {
+      id: "1",                                      - string
+      from: "1",                                    - string
+      to: "2",                                      - string
+      label: "1",                                   - string
+      smooth: { type: "curvedCW", roundness: 0.2 }  - object (curve functions) (optional field, absence of field will result in straight line)
+    },
+ */
+
 export interface automataData {
   nodes: DataSet<any, any>;
   edges: DataSet<any, any>;
+}
+
+export interface dfaResponse {
+  valid: boolean;
+  accepted: boolean;
 }
 
 /**
@@ -11,7 +36,7 @@ export interface automataData {
  * @param data
  * @return boolean
  */
-function isAutomataData(data: any) {
+function isAutomataData(data: any): boolean {
   if ("nodes" in data && "edges" in data) {
     if (data.nodes instanceof DataSet && data.edges instanceof DataSet) {
       return true;
@@ -21,33 +46,99 @@ function isAutomataData(data: any) {
 }
 
 /**
+ * returns true if every edge are unique i.e unique from and label combination
+ *
+ * @param edges
+ * @return boolean
+ */
+function isUniqueEdges(edges: Array<any>): boolean {
+  const edgesLength = edges.length;
+  for (let edgeIndex = 0; edgeIndex < edgesLength; edgeIndex += 1) {
+    const edge = edges[edgeIndex];
+    for (
+      let searchIndex = edgeIndex + 1;
+      searchIndex < edgesLength;
+      searchIndex += 1
+    ) {
+      const searchEdge = edges[searchIndex];
+      if (edge.from === searchEdge.from && edge.label === searchEdge.label) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * checks if network is valid DFA
+ *
+ * @param nodes
+ * @param edges
+ * @return boolean
+ */
+function isValidDFA(nodes: Array<any>, edges: Array<any>): boolean {
+  /**
+   * DFA edges should be deterministic.
+   * For a given state and given input alphabet there should be only one edge
+   */
+  return isUniqueEdges(edges);
+}
+
+/**
+ * Generate response object
+ *
+ * @param valid
+ * @param accepted
+ */
+function generateResponse(valid: boolean, accepted: boolean): dfaResponse {
+  const response = {
+    valid: false,
+    accepted: false
+  };
+
+  response.valid = valid;
+  response.accepted = accepted;
+
+  return response;
+}
+
+/**
  * Method accepts Automaton DataSet and input string.
- * Returns true or false based on if the automaton accepts the input string
+ *
+ * Returns an object with following keys
+ *
+ * valid : key denotes if the given network is a valid DFA
+ *
+ * accepted : key denotes if for the given input the network(DFA) accepts it
  *
  * @param inputString - string to check if it's a valid string
  * @param data - The automaton details. It should be in the form of <pre>{ "nodes": nodes; "edges": edges }</pre>
- * @return boolean - true if automaton accepts the input string false otherwise
+ * @return dfaResponse - <pre>{valid: boolean, accepted: boolean}</pre>
  * @throws TypeError
  */
-function computeDFA(inputString: string, data: automataData): boolean {
+function computeDFA(inputString: string, data: automataData): dfaResponse {
   if (isAutomataData(data)) {
-    const nodes = data.nodes.get();
-    const nodesLength = nodes.length;
-    const edges = data.edges.get();
-    const edgesLength = edges.length;
-    const inputStringLength = inputString.length;
-    let currentNode = "1";
-
     /**
      * TODO:
      * 1. DataSet schema validation
      */
 
+    const nodes = data.nodes.get();
+    const nodesLength = nodes.length;
+    const edges = data.edges.get();
+    const edgesLength = edges.length;
+
+    const isValid = isValidDFA(nodes, edges);
+    if (!isValid) return generateResponse(isValid, false);
+
+    const inputStringLength = inputString.length;
+    let currentNode = "1";
+
     for (let inputIndex = 0; inputIndex < inputStringLength; inputIndex += 1) {
       const inputChar = inputString[inputIndex];
       /**
-       *  let suppose for this inputChar we don't have a valid edge with the given currentNode.
-       *  It's trivial we shouldn't be traversing further
+       *  let suppose for the given input alphabet we don't have a valid edge with the given node.
+       *  we shouldn't be traversing further
        */
       let toContinueSearch = false;
       for (let edgeIndex = 0; edgeIndex < edgesLength; edgeIndex += 1) {
@@ -59,7 +150,7 @@ function computeDFA(inputString: string, data: automataData): boolean {
         }
       }
       if (!toContinueSearch) {
-        return false;
+        return generateResponse(isValid, false);
       }
     }
 
@@ -69,10 +160,10 @@ function computeDFA(inputString: string, data: automataData): boolean {
     for (let index = 0; index < nodesLength; index += 1) {
       const node = nodes[index];
       if (node.id === currentNode && node.final) {
-        return true;
+        return generateResponse(isValid, true);
       }
     }
-    return false;
+    return generateResponse(isValid, false);
   }
   throw new TypeError("function only accepts Dataset");
 }
