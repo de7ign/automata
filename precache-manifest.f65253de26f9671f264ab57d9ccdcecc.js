2f8eba336ef544e0c8fe931943d962953a2a1cba
Nihal Murmu <nhlmrm@gmail.com>
2020-04-19 22:29:53 +0530

    Updates

---
+++ precache-manifest.f65253de26f9671f264ab57d9ccdcecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2012d2a8e9c152cf852602c3c65d6961",
+    "url": "/automata/index.html"
+  },
+  {
+    "revision": "b23958fe581ff150b3c8",
+    "url": "/automata/static/css/main.a1ff8d39.chunk.css"
+  },
+  {
+    "revision": "27d34fff1eb3e5737463",
+    "url": "/automata/static/js/2.ddba0cc2.chunk.js"
+  },
+  {
+    "revision": "e75a1be83a391f33b3f54fe1de9af78d",
+    "url": "/automata/static/js/2.ddba0cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23958fe581ff150b3c8",
+    "url": "/automata/static/js/main.f3912ecb.chunk.js"
+  },
+  {
+    "revision": "2239020ba0e69cdd543a",
+    "url": "/automata/static/js/runtime-main.413cfa70.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/automata/precache-manifest.20769365fc384c6bd547bc45580d27b3.js"
+  "/automata/precache-manifest.f65253de26f9671f264ab57d9ccdcecc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/automata/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/automata/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
