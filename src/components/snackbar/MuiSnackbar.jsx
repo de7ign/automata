271fca61058f4ad0001fdfe2975027cc36fdc327
Nihal Murmu <nhlmrm@gmail.com>
2020-05-02 01:41:31 +0530

    chore() - Refactor snackbar (#116)
    remove the unnecessary snackbar wrapper functions

--- src/components/snackbar/MuiSnackbar.jsx
+++ src/components/snackbar/MuiSnackbar.jsx
@@ -18,12 +18,7 @@ const MuiSnackbar = forwardRef((props, ref) => {
      */
     openSnackbar: (variant, message) => {
       const allowedVariant = ["error", "warning", "info", "success"];
-      if (allowedVariant.indexOf(variant) === -1) {
-        throw new Error(
-          "Invalid variant Type, allowed variant types are 'error', 'warning', 'info', 'success'"
-        );
-      }
-      if (open) return;
+      if (open || allowedVariant.indexOf(variant) === -1) return;
       setAlertData({
         variant,
         message
