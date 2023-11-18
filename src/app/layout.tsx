ae2ab012b67354a34cda83586497511adebac1b9
Nihal Murmu <nhlmrm@gmail.com>
2023-11-19 00:25:08 +0530

    Add shadcnui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "../lib/utils"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +24,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+        {children}
+      </body>
     </html>
   )
 }
