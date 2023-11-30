c9ed39a97b885c6a65422639feb121f28ce8be67
Nihal Murmu <nhlmrm@gmail.com>
2023-12-01 01:25:49 +0530

    feat(): Add state context menu template

--- src/components/workspace-canvas.tsx
+++ src/components/workspace-canvas.tsx
@@ -1,24 +1,70 @@
 'use client'
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DataSet } from "vis-data/peer"
-import { Network, Options } from "vis-network/peer"
+import { IdType, Network, Options, Position } from "vis-network/peer"
 import {
   Card,
   CardContent
 } from "@/components/ui/card"
+import keycharm, { Keycharm } from "keycharm";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "./ui/context-menu";
 
+type ContextMenu = 'addNode' | 'updateNode' | null;
+
+interface NetworkEventParams {
+  edges: string[];
+  event: PointerEvent;
+  nodes: string[];
+  pointer: {
+    DOM: Position;
+    canvas: Position;
+  }
+}
 
 export default function AutomataWorkspaceCanvas() {
 
-  const container = useRef<HTMLDivElement>(null);
+  const networkContainer = useRef<HTMLDivElement>(null);
   let network: Network
+  let keyBinding: Keycharm
+
+  const [contextMenuMode, setContextMenuMode] = useState<ContextMenu>(null);
 
   useEffect(() => {
-    if (container.current) {
-      network = new Network(container.current, data, options);
+    if (networkContainer.current) {
+      network = new Network(networkContainer.current, data, options);
+
+      network.on("doubleClick", (params: NetworkEventParams) => {
+        addNode(params);
+      });
+
+      network.on('oncontext', (params: NetworkEventParams) => {
+        const nodeId: IdType = network.getNodeAt(params.pointer.DOM)
+        if (nodeId) {
+          network.selectNodes([nodeId], false);
+          setContextMenuMode("updateNode")
+        } else {
+          setContextMenuMode("addNode")
+        }
+
+      })
+
+      keyBinding = keycharm({
+        container: networkContainer.current,
+        preventDefault: true
+      })
     }
-  }, [container])
+
+    // unbind as part of cleanup
+    return () => {
+
+    }
+  }, [networkContainer])
+
+
+  function addNode(params: any): void {
+    alert('double clicked')
+  }
 
   const nodes = new DataSet([
     { id: 1, label: 'Node x' },
@@ -31,9 +77,9 @@ export default function AutomataWorkspaceCanvas() {
   // create an array with edges
   const edges = new DataSet([
     { id: 1, from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 }
+    { id: 2, from: 1, to: 2 },
+    { id: 3, from: 2, to: 4 },
+    { id: 4, from: 2, to: 5 }
   ]);
 
   // provide the data in the vis format
@@ -73,11 +119,34 @@ export default function AutomataWorkspaceCanvas() {
 
   // initialize your network!
 
+  function onContextMenuOpenChange(open: boolean): void {
+
+    if(!open) {
+      setContextMenuMode(null);
+    }
+  }
+
+
   return (
-    <Card className="lg:h-[800px]">
+    <Card className="lg:h-[800px] w-9/12">
       <CardContent className="h-full p-0">
-        <div ref={container} className="h-full">
-        </div>
+        <ContextMenu onOpenChange={onContextMenuOpenChange}>
+          <ContextMenuTrigger>
+            <div ref={networkContainer} className="h-full"></div>
+          </ContextMenuTrigger>
+          {contextMenuMode === 'addNode' && (<ContextMenuContent>
+            <ContextMenuItem>Add state</ContextMenuItem>
+            <ContextMenuItem>Add start state</ContextMenuItem>
+            <ContextMenuItem>Add final state</ContextMenuItem>
+          </ContextMenuContent>
+          )}
+          {contextMenuMode === 'updateNode' && (<ContextMenuContent>
+            <ContextMenuItem>Edit state label</ContextMenuItem>
+            <ContextMenuItem>Delete node</ContextMenuItem>
+          </ContextMenuContent>
+          )}
+        </ContextMenu>
+
       </CardContent>
     </Card>
   )
