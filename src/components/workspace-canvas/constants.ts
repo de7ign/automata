2b4e4933d2ff854b06deffff2661a71665e8f223
Nihal Murmu <31739586+de7ign@users.noreply.github.com>
2024-04-13 17:17:06 +0530

    Update root theme and canvas nodes/edges to similar

--- src/components/workspace-canvas/constants.ts
+++ src/components/workspace-canvas/constants.ts
@@ -6,6 +6,18 @@ export const NETWORK_DEFAULT_OPTION: Options = {
     widthConstraint: {
       minimum: 50,
       maximum: 50
+    },
+    color: {
+      border: '#2563eb',
+      background: '#D3E0FB',
+      highlight: {
+        border: '#2563eb',
+        background: '#E4ECFC'
+      },
+      hover: {
+        border: '#2563eb',
+        background: '#E4ECFC'
+      }
     }
   },
   edges: {
@@ -13,7 +25,12 @@ export const NETWORK_DEFAULT_OPTION: Options = {
       to: { enabled: true, scaleFactor: 1, type: "arrow" }
     },
     // by default all edges property should be this
-    smooth: { enabled: true, type: "curvedCW", roundness: 0.5 }
+    smooth: { enabled: true, type: "curvedCW", roundness: 0.5 },
+    color: {
+      color:'#2563eb',
+      highlight:'#2563eb',
+      hover: '#2563eb',
+    }
   },
   physics: {
     enabled: false // should I enable it or add a functionality user can enable/disable physics ?
