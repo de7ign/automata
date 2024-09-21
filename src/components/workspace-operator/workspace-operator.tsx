56ddfb05b8bc2fb69ffdf1938e37088961cd6878
Nihal Murmu <31739586+de7ign@users.noreply.github.com>
2024-09-21 21:24:10 +0530

    feat: Add export to png button

--- src/components/workspace-operator/workspace-operator.tsx
+++ src/components/workspace-operator/workspace-operator.tsx
@@ -94,6 +94,26 @@ export default function WorkspaceOperator() {
     }
   };
 
+  const exportNetworkAsImage = () => {
+    // We only have one canvas, so can query directly
+    const canvas = document.querySelector('canvas');
+  
+    if (canvas) {
+      // Convert the canvas to a data URL (Base64 image)
+      const dataURL = canvas.toDataURL();
+  
+      // Create a download link
+      const link = document.createElement('a');
+      link.href = dataURL;
+      link.download = 'network.png';
+  
+      // Programmatically click the link to trigger the download
+      link.click();
+    } else {
+      console.error('Canvas not found!');
+    }
+  }
+
   return (
     <Card className="lg:h-[800px] w-3/12">
       <CardHeader>
@@ -209,7 +229,9 @@ export default function WorkspaceOperator() {
               Export
             </AccordionTrigger>
             <AccordionContent>
-              Coming soon
+              <div className="m-1">
+                <Button onClick={exportNetworkAsImage}>Export as PNG</Button>
+              </div>
             </AccordionContent>
           </AccordionItem>
         </Accordion>
