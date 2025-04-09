dc92712da778843fcead36fbe16430393463a130
Nihal Murmu <31739586+de7ign@users.noreply.github.com>
2025-04-10 01:03:56 +0530

    feat: Add export as jpeg

--- src/components/workspace-operator/workspace-export/workspace-export.tsx
+++ src/components/workspace-operator/workspace-export/workspace-export.tsx
@@ -2,18 +2,22 @@ import { Button } from "@/components/ui/button";
 
 export default function WorkspaceExport() {
 
-  const exportNetworkAsImage = () => {
+  const exportNetworkAsImage = (type: string) => {
     // We only have one canvas, so can query directly
     const canvas = document.querySelector('canvas');
 
     if (canvas) {
       // Convert the canvas to a data URL (Base64 image)
-      const dataURL = canvas.toDataURL();
+      const dataURL = canvas.toDataURL(type);
 
       // Create a download link
       const link = document.createElement('a');
       link.href = dataURL;
-      link.download = 'network.png';
+      if (type === 'image/png') {
+        link.download = 'network.png';
+      } else if (type === 'image/jpeg') {
+        link.download = 'network.jpeg';
+      }
 
       // Programmatically click the link to trigger the download
       link.click();
@@ -23,8 +27,9 @@ export default function WorkspaceExport() {
   }
 
   return (
-    <div className="m-1">
-      <Button onClick={exportNetworkAsImage}>Export as PNG</Button>
+    <div className="flex 2xl:flex-row xl:flex-col gap-4 m-1">
+      <Button onClick={() => exportNetworkAsImage('image/png')}>Export as PNG</Button>
+      <Button onClick={() => exportNetworkAsImage('image/jpeg')}>Export as JPEG</Button>
     </div>
   )
 }
